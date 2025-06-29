da56b50aadf2e1b681449ef597a3d3da666e3e5f
apple-yagi <applesryu@gmail.com>
2025-06-29 13:30:27 +0900

    Setup action

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import { getChangedFiles } from "./get-changed-files.ts";
 import { getAffectedTestFiles } from "./get-affected-tests.ts";
+import { getInput, setFailed, setOutput } from "@actions/core";
+import path from "node:path";
 
-const changedFiles = getChangedFiles("origin/main");
-const testFiles = getAffectedTestFiles(changedFiles);
+const run = async (tsconfig: string) => {
+  const changedFiles = getChangedFiles(tsconfig);
+  const affectedTestFiles = getAffectedTestFiles(
+    changedFiles,
+    path.resolve(tsconfig)
+  );
 
-console.log(testFiles.join("\n"));
+  return affectedTestFiles;
+};
+
+(async () => {
+  try {
+    const tsconfig = getInput("tsconfig") || "tsconfig.json";
+    const results = await run(tsconfig);
+
+    if (results.length === 0) {
+      console.log("No affected test files found.");
+      return;
+    }
+
+    setOutput("affected_tests", results.join(" "));
+  } catch (err) {
+    if (err instanceof Error) setFailed(err.message);
+  }
+})();
